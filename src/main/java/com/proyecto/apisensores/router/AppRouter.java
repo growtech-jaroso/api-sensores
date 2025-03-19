87bd2d434b3776d2d6d82462b4893d0d4d5d3f4a
Comparitiko <gabigcy@gmail.com>
2025-03-19 18:36:48 +0100

    App router hechas importaciones

--- src/main/java/com/proyecto/apisensores/router/AppRouter.java
+++ src/main/java/com/proyecto/apisensores/router/AppRouter.java
@@ -2,7 +2,9 @@ package com.proyecto.apisensores.router;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
@@ -10,12 +12,12 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 @Configuration
 public class AppRouter {
   @Bean
-  public RouterFunction<ServerResponse> appRoutes(ProductRouter productRouter) {
+  public RouterFunction<ServerResponse> appRoutes(PlantationRouter plantationRouter) {
     return RouterFunctions.route()
       // Base API prefix
       .path("/api", builder -> builder
         .nest(accept(MediaType.APPLICATION_JSON), nestedBuilder ->
-          nestedBuilder.path("/products", productRouter::productRoutes) // Routes for products
+          nestedBuilder.path("/products", plantationRouter::plantationRoutes) // Routes for products
         )
       )
       .build();
