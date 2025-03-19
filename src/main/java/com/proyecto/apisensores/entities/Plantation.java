ee6d36546912c2bd76774493ffa643c575f79ba0
alxrgdriguez <sralexred@gmail.com>
2025-03-19 17:41:42 +0100

    Plantation Entity

---
+++ src/main/java/com/proyecto/apisensores/entities/Plantation.java
@@ -0,0 +1,36 @@
+package com.proyecto.apisensores.entities;
+
+import jakarta.persistence.Column;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import  lombok.*;
+import java.util.List;
+
+@Document(collation = "plantations")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Plantation extends Model  {
+
+  @Column(unique = true)
+  private String name; // Hace que el campo "name" sea único
+  private String country;
+  private String province;
+  private String city;
+  private String type;
+
+  public Plantation(String name, String country, String province, String city, String type) {
+    this.name = name;
+    this.country = country;
+    this.province = province;
+    this.city = city;
+    this.type = type;
+  }
+
+  // Relación con usuarios (Lista de IDs de usuarios asociados)
+  private List<String> users;
+
+}
