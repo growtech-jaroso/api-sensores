2dde99f24f1d96c933fc156f343af69a36b63eb9
Comparitiko <gabigcy@gmail.com>
2025-04-26 11:18:46 +0200

    Add conversion methods for SensorType and SensorUnit enums

--- src/main/java/com/proyecto/apisensores/enums/SensorType.java
+++ src/main/java/com/proyecto/apisensores/enums/SensorType.java
@@ -3,5 +3,19 @@ package com.proyecto.apisensores.enums;
 public enum SensorType {
   TEMPERATURE,
   HUMIDITY,
-  PRESSURE,
+  PRESSURE;
+
+  /**
+   * Convert a string to a SensorType enum.
+   * @param sensorTypeStr string representation of the sensor type
+   * @return the corresponding SensorType enum
+   */
+  public static SensorType convertFromString(String sensorTypeStr) {
+      try {
+          return SensorType.valueOf(sensorTypeStr.toUpperCase());
+      } catch (IllegalArgumentException e) {
+          // Handle the case where the string does not match any enum constant returning null
+          return null;
+      }
+  }
 }
--- src/main/java/com/proyecto/apisensores/enums/SensorUnit.java
+++ src/main/java/com/proyecto/apisensores/enums/SensorUnit.java
@@ -2,5 +2,18 @@ package com.proyecto.apisensores.enums;
 
 public enum SensorUnit {
   CELSIUS,
-  FAHRENHEIT,
+  PERCENTAGE;
+
+  /**
+   * Convert a string to a SensorUnit enum.
+   * @param sensorUnitStr string representation of the sensor type
+   * @return the corresponding SensorType enum
+   */
+  public static SensorUnit convertFromString(String sensorUnitStr) {
+    try {
+      return SensorUnit.valueOf(sensorUnitStr.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      return null;
+    }
+  }
 }
