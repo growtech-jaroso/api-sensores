3a63b325280b8e68119cb113b7b98718f579ce9c
Comparitiko <gabigcy@gmail.com>
2025-05-02 20:03:19 +0200

    refactor: rename ApiSensoresApplication to GrowTechApplication

--- src/main/java/com/growtech/api/GrowTechApplication.java
+++ src/main/java/com/growtech/api/GrowTechApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiSensoresApplication {
+public class GrowTechApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiSensoresApplication.class, args);
+        SpringApplication.run(GrowTechApplication.class, args);
     }
 
 }
