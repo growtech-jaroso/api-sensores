abfc4287be8dc727fe6af3bbb4a9415f8e4ec2d5
AdrianPerez2207 <aperezsanchez441@gmail.com>
2025-05-04 12:39:27 +0200

    sensor value

--- src/main/java/com/growtech/api/repositories/SensorValueRepository.java
+++ src/main/java/com/growtech/api/repositories/SensorValueRepository.java
@@ -1,7 +1,25 @@
 package com.growtech.api.repositories;
 
 import com.growtech.api.entities.SensorValue;
+import com.growtech.api.enums.SensorType;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
 
 public interface SensorValueRepository extends ReactiveMongoRepository<SensorValue, String> {
+  Flux<SensorValue> findBySensorId(String sensorId);
+
+  Flux<SensorValue> findByValue(Double value);
+
+  Flux<SensorValue> findBySensorType(SensorType sensorType);
+
+  Flux<SensorValue> findBySensorTypeAndTimestampBetween(SensorType sensorType, LocalDateTime timestampAfter, LocalDateTime timestampBefore);
+
+  Flux<SensorValue> findByTimestampBetween(LocalDateTime timestamp, LocalDateTime timestamp2);
+
+  Flux<SensorValue> findByIdAndTimestampBetween(String id, LocalDateTime timestampAfter, LocalDateTime timestampBefore);
+
+  Flux<SensorValue> findBySensorIdOrderByTimestamp(String sensorId);
 }
+
