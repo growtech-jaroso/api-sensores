8df726fd010fff943142eac56350f8d87536970e
Comparitiko <gabigcy@gmail.com>
2025-05-10 12:03:53 +0200

    feat: update User constructor to accept encrypted password for enhanced security

--- src/main/java/com/growtech/api/entities/User.java
+++ src/main/java/com/growtech/api/entities/User.java
@@ -52,10 +52,10 @@ public class User extends Model implements UserDetails {
     this.role = UserRole.USER;
   }
 
-  public User(UserRegisterDto dto, String password) {
+  public User(UserRegisterDto dto, String encryptedPassword) {
     super();
     this.username = dto.username();
-    this.password = password;
+    this.password = encryptedPassword;
     this.email = dto.email();
     this.role = UserRole.convertFromString(dto.role());
   }
